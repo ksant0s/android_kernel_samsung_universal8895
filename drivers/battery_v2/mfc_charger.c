d1f347ad7cb01ed70b1a42db4ee507a08414cb01
Michael Benedict <michaelbt@live.com>
2019-06-03 18:21:08 +1000

    battery_v2: silence enum-conversion warnings
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- drivers/battery_v2/mfc_charger.c
+++ drivers/battery_v2/mfc_charger.c
@@ -1809,7 +1809,7 @@ static int mfc_chg_get_property(struct power_supply *psy,
 		union power_supply_propval *val)
 {
 	struct mfc_charger_data *charger = power_supply_get_drvdata(psy);
-	enum power_supply_ext_property ext_psp = psp;
+	enum power_supply_ext_property ext_psp = (enum power_supply_ext_property) psp;
 //	union power_supply_propval value;
 	u8 mst_mode;
 	u8 reg_data;
@@ -2060,7 +2060,7 @@ static int mfc_chg_set_property(struct power_supply *psy,
 		const union power_supply_propval *val)
 {
 	struct mfc_charger_data *charger = power_supply_get_drvdata(psy);
-	enum power_supply_ext_property ext_psp = psp;
+	enum power_supply_ext_property ext_psp = (enum power_supply_ext_property) psp;
 	int vout, vrect, iout, freq, i = 0;
 	u8 tmp = 0;
 	/* int ret; */
